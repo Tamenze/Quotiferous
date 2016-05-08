422be218fc8aaf1b155fb689fa0ecc10e3a367c2
Tolulope <tamenze@gmail.com>
2016-05-08 07:07:14 -0400

    Added hover citation and new gradient

--- main.js
+++ main.js
@@ -7,21 +7,29 @@
 $(document).ready(function(){
 
 
-var yurps = [
+var remarks = [
 "Swag.",
 "Don't quit.",
 "Seek self-improvement.",
 "To not act is to be amongst the living dead.",
 "You made the right plans, now all you have to do is keep doing what you're doing.",
 "Not only is it possible for you to have your dream, but it is necessary.",
-"Every person is the form taken by a stream -- a marvelous torrent of milk, water, bread, beefsteak, fruit, vegetables, air, light, radiation -- all of which are streams in their own turn.",
-"How is it possible that a being with such sensitive jewels as the eyes, such enchanted musical instruments as the ears, and such a fabulous arabesque of nerves as the brain can experience itself as anything less than a god? And, when you consider that this incalculably subtle organism is inseparable from the still more marvelous patterns of its environment--from the minutest electrical designs to the whole company of the galaxies--how is it conceivable that this incarnation of all eternity can be bored with being?",
-"You need the tiniest bit of bravery.",
-"Work hard, know your shit, show your shit, and then feel entitled. Listen to no one except the two smartest and kindest adults you know, and that doesn't aways mean your parents. If you do that, you will be fine.",
-"Sometimes, I feel discriminated against, but it does not make me angry. It merely astonishes me. How can any deny themselves the pleasure of my company? It’s beyond me.",
-"If you've got it, flaunt it. And if you don't got it? Flaunt it. 'Cause what are we even doing here if we're not flaunting it?",
-"Jack fought down the guilty overwhelming surge of relief, that he was not going to have to marry this beautiful dead girl and spend the rest of his life with her.",
-"Sweet like licorice, dangerous like syphillis."
+
+"<span title='Alan Watts'> Every person is the form taken by a stream -- a marvelous torrent of milk, water, bread, beefsteak, fruit, vegetables, air, light, radiation -- all of which are streams in their own turn. <span>",
+
+"<span title='Alan Watts'> How is it possible that a being with such sensitive jewels as the eyes, such enchanted musical instruments as the ears, and such a fabulous arabesque of nerves as the brain can experience itself as anything less than a god? And, when you consider that this incalculably subtle organism is inseparable from the still more marvelous patterns of its environment--from the minutest electrical designs to the whole company of the galaxies--how is it conceivable that this incarnation of all eternity can be bored with being? <span>",
+
+"<span title = 'Mindy Kaling'> You need the tiniest bit of bravery. <span>",
+
+"<span title = 'Mindy Kaling'> Work hard, know your shit, show your shit, and then feel entitled. Listen to no one except the two smartest and kindest adults you know, and that doesn't aways mean your parents. If you do that, you will be fine. <span>",
+
+"<span title = 'Zora Neale Hurston'> Sometimes, I feel discriminated against, but it does not make me angry. It merely astonishes me. How can any deny themselves the pleasure of my company? It’s beyond me. <span>",
+
+"<span title = 'Mindy Kaling'> If you've got it, flaunt it. And if you don't got it? Flaunt it. 'Cause what are we even doing here if we're not flaunting it? <span>",
+
+"<span title = 'Maeve Binchy'> Jack fought down the guilty overwhelming surge of relief, that he was not going to have to marry this beautiful dead girl and spend the rest of his life with her. <span>",
+
+"<span title = 'Lauryn Hill'> Sweet like licorice, dangerous like syphillis. <span>"
 ];
 
 var graphics = [
@@ -31,14 +39,16 @@ var graphics = [
 	"linear-gradient(to bottom right, #1A0833, #441487, #7924F2)",
 	"linear-gradient(to bottom left, #48A80F, #48A893, #48A8FF)",
 	"linear-gradient(to top left, #F5195C, #90195C, #18195C)",
-	"linear-gradient(to right, #009343, #839343, #FE9343)"
+	"linear-gradient(to right, #009343, #839343, #FE9343)",
+	"linear-gradient(45deg, rgba(255,13,227,1) 0%, rgba(0,128,128,1) 100%)"
 ];
 
 
 
-	$("#quote_text").html(yurps[Math.floor(Math.random()*(yurps.length))]);
+
+	$("#quote_text").html(remarks[Math.floor(Math.random()*(remarks.length))]);
 	$(".still_good_tho").css("background-image", graphics[Math.floor(Math.random()*(graphics.length))]);
-   	console.log("what's good?")
+   	// console.log("what's good?")
 
 $("#refresh").click(function(){
 	document.location.reload(true);
